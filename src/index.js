f1d43cf1f67dfee39759e00b56fc5a8a3f4675ba
yveso <yveso@users.noreply.github.com>
2019-12-05 13:40:56 +0100

    Track State with React.useState

--- src/index.js
+++ src/index.js
@@ -3,16 +3,25 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const pokemons = [
+  { name: "Bisasam" },
+  { name: "Bisaknosp" },
+  { name: "Bisaflor" }
+];
+
 function Pokemon({ name, ...rest }) {
   return <h1 {...rest}>{name}</h1>;
 }
 
 function App() {
+  const [index, setIndex] = React.useState(0);
+
   return (
     <div>
-      <Pokemon name="Bisasam" class="Whut" />
-      <Pokemon name="Bisaknosp" id="23" />
-      <Pokemon name="Bisaflor" data-something="nice" />
+      <button type="button" onClick={() => setIndex(index + 1)}>
+        Next
+      </button>
+      <Pokemon name={pokemons[index].name} />
     </div>
   );
 }
