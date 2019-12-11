410e619416e67ab6a29a46f94bcd223d54062551
yveso <yveso@users.noreply.github.com>
2019-12-11 22:07:41 +0100

    As Component Prop in React

--- src/index.js
+++ src/index.js
@@ -28,6 +28,16 @@ function Pokemon({ name, ...rest }) {
   return <h1 {...rest}>{name}</h1>;
 }
 
+function PokemonList({ as: As, items, ...rest }) {
+  return (
+    <As {...rest}>
+      {items.map(pokemon => (
+        <li key={pokemon.name}>{pokemon.name}</li>
+      ))}
+    </As>
+  );
+}
+
 function App() {
   const [index, setIndex] = React.useState(0);
   const pokemon = usePokemon(index);
@@ -46,11 +56,7 @@ function App() {
       )}
 
       {collection ? (
-        <ul>
-          {collection.results.map(pokemon => (
-            <li key={pokemon.name}>{pokemon.name}</li>
-          ))}
-        </ul>
+        <PokemonList as="ol" items={collection.results} />
       ) : (
         <div>Fetching Pokemon</div>
       )}
